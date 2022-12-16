17dd704c5bb96d8203e7accc1bbd4d2486d9b588
cshujun23 <c.shujun23@gmail.com>
2022-12-16 21:35:31 +0800

    Sprite sheet linked

--- script.js
+++ script.js
@@ -16,7 +16,23 @@ const pageLoaded = () => {
   //2d context objects provide methods to draw game elements on screen
   const ctx = canvas.getContext("2d");
 
+  //Get sprites
+
+  const spriteSheet = document.querySelector("sprite-sheet");
+
   //Drawing code here:
+  // Draw a sprite from the sprite sheet onto the canvas
+  context.drawImage(
+    spriteSheet,
+    x,
+    y,
+    width,
+    height,
+    canvasX,
+    canvasY,
+    width,
+    height
+  );
 };
 
 const createRow = () => {};
@@ -25,3 +41,5 @@ const createRow = () => {};
 //rythm 1: 11231
 //rythm 2: 111321
 //rythm 3: 11132111
+
+console.log("no bug");
