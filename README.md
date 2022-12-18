a33440525dd3f8474b1f2c7cf88c973351d87d1c
cshujun23 <c.shujun23@gmail.com>
2022-12-18 11:12:56 +0800

    Created game icons

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Dancing-Game-
-Dancing game inspired by retro game, Tero Beats PSX Bishi Bashi
+
+Dancing game inspired by retro PSX game, Bishi Bashi Perm Mania
+
+References
+
+Stage image
+https://www.pngegg.com/en/png-bzgte
+
+Sprite sheet
+https://www.spriters-resource.com/playstation/bishibashispecialjpn/sheet/7677/
+
+Game buttons set
+https://pngtree.com/so/set
--- script.js
+++ script.js
@@ -3,43 +3,37 @@
 //see if match on beat
 //reaction - audio visual
 
+//melody1 = 11231
+const rhythm1 = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0];
+//melody2 = 111321
+const rhythm2 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0];
+//melody3 = 11132111
+const rhythm3 = [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0];
+
 //This function will be called once the page loads completely
 const pageLoaded = () => {
-  alert("Hello Player!");
-
+  // alert("Hello Player!");
   //Get a handle to the canvas object
-  const canvas = document.querySelector("#myCanvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
+  const canvas = document.querySelector("canvas");
   //Get the 2d context for this canvas
   //2d context objects provide methods to draw game elements on screen
   const ctx = canvas.getContext("2d");
-
   //Get sprites
-
-  const spriteSheet = document.querySelector("sprite-sheet");
-
-  //Drawing code here:
+  const spriteSheet = document.querySelector("#sprite-sheet");
+  //DRAWING code here:
   // Draw a sprite from the sprite sheet onto the canvas
-  context.drawImage(
-    spriteSheet,
-    x,
-    y,
-    width,
-    height,
-    canvasX,
-    canvasY,
-    width,
-    height
-  );
+  // context.drawImage(
+  //   spriteSheet,
+  //   x,
+  //   y,
+  //   width,
+  //   height,
+  //   canvasX,
+  //   canvasY,
+  //   width,
+  //   height
+  // );
+  const createRow = () => {};
 };
-
-const createRow = () => {};
-
-//Audio
-//rythm 1: 11231
-//rythm 2: 111321
-//rythm 3: 11132111
 
 console.log("no bug");
