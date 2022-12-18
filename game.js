9b6bb584e4a15edee86efa1c9362388eadf12b4f
cshujun23 <c.shujun23@gmail.com>
2022-12-18 13:50:45 +0800

    Set game background

---
+++ game.js
@@ -0,0 +1,35 @@
+//melody1 = 11231
+const rhythm1 = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0];
+//melody2 = 111321
+const rhythm2 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0];
+//melody3 = 11132111
+const rhythm3 = [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0];
+
+//This function will be called once the page loads completely
+const pageLoaded = () => {
+  //Start initialising objects, preloading assets and display start screen
+  const game = {}
+  init: function(){
+
+  }
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+};
+
+console.log("no bug");
+
+// //Get sprites
+// const spriteSheet = document.querySelector("#sprite-sheet");
+// //DRAWING code here:
+// // Draw a sprite from the sprite sheet onto the canvas
+// // context.drawImage(
+// //   spriteSheet,
+// //   x,
+// //   y,
+// //   width,
+// //   height,
+// //   canvasX,
+// //   canvasY,
+// //   width,
+// //   height
+// // );
