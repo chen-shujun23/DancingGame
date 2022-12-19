d414a5af36def8d9cbddc78e20e125d81d2d5bc8
cshujun23 <c.shujun23@gmail.com>
2022-12-19 11:10:22 +0800

    Start screen layout done

--- game.js
+++ game.js
@@ -5,16 +5,58 @@ const rhythm2 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0];
 //melody3 = 11132111
 const rhythm3 = [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0];
 
-//This function will be called once the page loads completely
-const pageLoaded = () => {
-  //Start initialising objects, preloading assets and display start screen
-  const game = {}
-  init: function(){
+// //This function will be called once the page loads completely
+// const pageLoaded = () => {
+//   //Start initialising objects, preloading assets and display start screen
+//   //Create game object with key-value pairs that saves references to game
+//   const game = {
+//     function init:() {
+//       const canvas = document.querySelector("canvas");
+//       const ctx = canvas.getContext("2d");
+//       //Hide all game layers and display the start screen
+//       hideScreens();
+//       showScreen("#startScreen");
+//     }
+//   }
+// };
 
-  }
-  const canvas = document.querySelector("canvas");
+//Function to start initialising objects, preloading assets and display start screen
+function init() {
+  const canvas = document.querySelector("#gameCanvas");
   const ctx = canvas.getContext("2d");
-};
+  //Hide all game layers and display the start screen
+  clearScreens();
+  showScreen("startScreen");
+}
+
+//Iterate through all the game layers and set their display to none
+function clearScreens() {
+  const screenArr = [document.querySelectorAll("gameLayer")];
+  for (let i = 0; i <= screen.length; i++) {
+    const screen = screenArr[i];
+    screen.style.display = "none";
+  }
+}
+//Function to hide game layer by ID
+function hideScreen(id) {
+  const screen = document.getElementById(id);
+  screen.style.display = "none";
+}
+
+//Function to show game layer by ID
+function showScreen(id) {
+  const screen = document.getElementById(id);
+  screen.style.display = "block";
+}
+
+//Function to play audio
+function playAudio(id) {
+  const audio = document.getElementById(id);
+  audio.play();
+}
+
+//Initialise game once page has fully loaded
+window.addEventListener("load", init);
 
 console.log("no bug");
 
