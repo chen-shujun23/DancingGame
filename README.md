433dba4ddf385d65e7627d6e3f28166c644c08db
cshujun23 <c.shujun23@gmail.com>
2022-12-23 04:03:01 +0800

    PROJECT 1 DONE

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
-# Dancing-Game-
+# Retro Beats
 
-Dancing game inspired by retro PSX game, Bishi Bashi Perm Mania
+This is a dancing game inspired by retro PSX game, Bishi Bashi Perm Mania, which challenges the player's sense of rhythm. The player listens to the melody once first to get a sense of the rhythm. Immediately after, the player needs to replicate the rhythm by tapping on the 'enter' key. With each beat on point, the player is awarded 1 point. There are 3 melodies to dance to. To win, the player needs to score above 10 upon 19 points. Dancing characters are animated to dance according to the rhythms and beats.
+
+## Technologies Used
+
+1. Visual Studio Code for scripting
+2. Adobe PhotoShop for editing images
+3. Garageband for making background music
+4. iMovie for editing instruction video
+5. **[Pine Tools](https://pinetools.com/pixelate-effect-image)** for achieving pixelated effect on images
+6. **[Piskel](https://www.piskelapp.com/p/create/sprite)** for creating GIFs
+7. **[Canva](https://www.canva.com/)** for making button images
+
+## General Approach
+
+**Object literal Programming**
+
+- Creating objects using object literals, which are collections of key-value pairs.
+
+### Breakdown of Approach
+
+1. **Global Variables:** Declare the rhythm of the 3 melodies using 1s and 0s in arrays. Iterate the arrays to get the precise timings the player must hit to score 1 point. Combine the precise timings into an array, which the player's result must tally with.
+2. **Root Game Object:** Create the top-level root game object that defines the entire scope of the game. This is the main composition object that initialises other objects' assets and methods. Top-level variables and methods for the game to run will be assigned to the game object, such as the methods needed to switch to the end screen after the game has ended.
+3. **Image & Audio Loader Object:** Create a loader object that can be used to load new images and audios in other objects. This will come in handy as the game expands and more assets need to be loaded.
+4. **Create other objects** like the instruction, level, scoreboard and ending objects. The loader's methods are used in these other objects to load audio and images. These objects are in turn initialised in the root game object.
+5. **Add Event Listeners** There are two main event listeners. The first listens for the pressing of the enter key, and if the timing matches the precise timings in point 1, the score will be updated in the scoreboard. Also, the image of the player character changes to animate the dance moves whenever the player hits the precise timing. The second listens for the end of the base audio track of the game. When the music stops, the game ends. Depending on the score, either the winning or losing screen will appear.
+
+### Reasons for Object Literal Approach:
+
+- **Simplicity**
+  - Object literals are a simple and concise way of creating objects.
+  - Do not require the use of constructor functions.
+  - Makes code easier to read and understand by compartmentalising code into objects
+- **Flexibitlity**
+  - Can easily create new objects to scale up the game, such as new levels with varying difficulties
+  - Can easily add and remove properties assigned to objects as needed
+
+## Challenges
+
+- Matching the animation with the audio
+- Animating the player character with each enter keydown; transition is still not smooth
+- Was not able to piece together the beat and melody using javascript without pauses between each audio
+
+## Unsolved Problems & Further Work
+
+1. Player's character flickers when it changes dance moves. This could be because it is animated by iteratimg and switching the image source url. This might be resolved by using a spritesheet of all the player images in one canvas, instead of using individual player images.
+2. To include a second player or allow the player to play against a pre-programmed computer.
+3. Consider different levels of difficulties.
+
+## References
+
+**Audio**
+
+- Background music remade according to the original **[Bishi Bashi Perm Mania](https://www.youtube.com/watch?v=l1MjKC9nDeM)**
+- **[Booing audio](https://pixabay.com/sound-effects/boo-6377/)**
+- **[Cheering audio](https://pixabay.com/sound-effects/short-crowd-cheer-6713/)**
+
+**Images**
+
+- **[Spritesheet](https://www.spriters-resource.com/fullview/7677/)**
+- **[Enter button image](http://pixelartmaker.com/art/b868ed6b29546ba)**
+- **[Crowd cheering image](https://favpng.com/png_view/crowd-cheering-applause-image-clip-art-clapping-png/ZYm8BHf9)**
+- **[Dance floor background](https://www.pngegg.com/en/png-bzgte)**
--- game.js
+++ game.js
@@ -1,35 +1,47 @@
 //===============================
-//Game global variables
+//Global variables
 //===============================
+
+//The music tempo is 120 beats per minute (bpm)
+//Each melody is 2 seconds long and has 1 bar of 4 notes. 1 note = 4 quarter-notes.
+//Hence, each rhythm array has 16 quarter-notes.
+//Each quarter-note takes 0.125 seconds
 
 const rhythm1 = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0];
 const rhythm2 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0];
 const rhythm3 = [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0];
 
+//Iterate throught the rhythm arrays to get the precise timing the player must hit to score
+//An array of precise rhythm timings for the 1st melody
 const rhythm1TimingArr = [];
 for (let i = 0; i < 16; i++) {
   if (rhythm1[i] === 1) {
+    //Add 4seconds as 4seconds in is when player's turn for melody 1 starts
+    //Mulitply by 0.125 as each quarter-note takes 0.125seconds
     const timing = 4 + (i + 1) * 0.125;
     rhythm1TimingArr.push(timing);
   }
 }
-
+//An array of precise rythm timings for the 2nd melody
 const rhythm2TimingArr = [];
 for (let i = 0; i < 16; i++) {
   if (rhythm2[i] === 1) {
+    //Add 8seconds as 8seconds in is when player's turn for melody 2 starts
     const timing = 8 + (i + 1) * 0.125;
     rhythm2TimingArr.push(timing);
   }
 }
-
+//An array of precise rythm timings for the 3rd melody.
 const rhythm3TimingArr = [];
 for (let i = 0; i < 16; i++) {
   if (rhythm3[i] === 1) {
+    //Add 16seconds as 16seconds in is when player's turn for melody 3 starts
     const timing = 12 + (i + 1) * 0.125;
     rhythm3TimingArr.push(timing);
   }
 }
-
+//Combine all the arrays to get a combined array of precise timings
+//This array is used to tally against the player's result
 const preciseTimingArr = [
   ...rhythm1TimingArr,
   ...rhythm2TimingArr,
@@ -39,7 +51,7 @@ const preciseTimingArr = [
 console.log(preciseTimingArr);
 
 //===============================
-//Main game object
+//Root game object
 //===============================
 
 //Initialise game once page has fully loaded
@@ -50,7 +62,7 @@ window.addEventListener("load", function () {
 const game = {
   //Method to start initialising objects, preloading assets and display start screen
   init: function () {
-    //Inititalise objects
+    //Inititalise other objects' properties
     instruction.init();
     level.init();
     scoreboard.init();
@@ -61,7 +73,30 @@ const game = {
     game.showScreen("startScreen");
   },
 
-  //Method to clear screens
+  //Game starts when player clicks Play
+  //The starting screen is cleared and the main playing screen(i.e. level screen) appears
+  //All assets assigned to the level screen are loaded
+  //Scoreboard is shown and the score updates as player plays
+  //Winning/losing screen shown at the end
+  start: function () {
+    game.clearScreens();
+    game.showScreen("levelScreen");
+    game.showScreen("scoreScreen");
+    level.load();
+    scoreboard.updateScore();
+    ending.winLose();
+  },
+
+  //Method to show the instructions when player clicks Menu
+  //At the end of the video instruction, the screen refreshes to the starting screen
+  instruct: function () {
+    instruction.load();
+    instruction.refreshScreen();
+    game.clearScreens();
+    game.showScreen("instructionScreen");
+  },
+
+  //Method to clear all screens
   clearScreens: function () {
     const screenArr = document.getElementsByClassName("gameLayer");
     for (let i = 0; i < screenArr.length; i++) {
@@ -75,60 +110,43 @@ const game = {
     const screen = document.getElementById(id);
     screen.style.display = "block";
   },
-
-  //Method to show instruction
-  instruct: function () {
-    instruction.load();
-    instruction.refreshScreen();
-    game.clearScreens();
-    game.showScreen("instructionScreen");
-  },
-
-  //Method  to start game
-  start: function () {
-    game.clearScreens();
-    game.showScreen("levelScreen");
-    game.showScreen("scoreScreen");
-    level.load();
-    scoreboard.updateScore();
-    ending.winLose();
-  },
 };
 
 //===============================
 //Image & audio loader object
 //===============================
 
-//Loader method for loading assets
+//Define an object 'loader' that contains
+//methods that alerts player if their browser does not support mp3 audio and mp4 videos
+//and load audio, image and video elements
 const loader = {
   init: function () {
-    let mp3Support;
-    if (audio.canPlayType("audio/mpeg") === "probably") {
-      mp3Support = false;
+    if (audio.canPlayType("audio/mpeg") !== "probably") {
       alert(
         "Your browser does not support mp3 audio. Please shift to a another browser"
       );
     }
-    let mp4Support;
-    if (video.canPlayType("audio/mpeg") === "probably") {
-      mp4Support = false;
+    if (video.canPlayType("audio/mpeg") !== "probably") {
       alert(
         "Your browser does not support mp4 video. Please shift to a another browser"
       );
     }
   },
+  //Define a 'loadSound' method that creates new audio elements
   loadSound: function (url, id) {
     const audio = document.createElement("audio");
     audio.src = url;
     audio.id = id;
     return audio;
   },
+  //Define a 'loadImage' method that creates new image elements
   loadImage: function (url, id) {
     let image = document.createElement("img");
     image.src = url;
     image.id = id;
     return image;
   },
+  //Define a 'loadVideo' method that creates new video elements
   loadVideo: function (url) {
     let source = document.createElement("source");
     source.src = url;
@@ -145,12 +163,14 @@ const instruction = {
     instructionScreen = document.getElementById("instructionScreen");
     video = document.getElementById("video");
   },
+  //Method to load the images and videos for the instructions page
   load: function () {
     videoSource = loader.loadVideo("video/instruction.mp4");
     video.appendChild(videoSource);
     enterGif = loader.loadImage("gif/enter.gif", "enterGif");
     instructionScreen.appendChild(enterGif);
   },
+  //Method to refresh to first page after 7 seconds, approximately when the instructional video ends
   refreshScreen: function () {
     setTimeout(function () {
       location.reload();
@@ -163,21 +183,22 @@ const instruction = {
 //===============================
 
 const level = {
-  //level data
   init: function () {
     levelScreen = document.getElementById("levelScreen");
     playerImgContainer = document.getElementById("playerImg");
   },
   load: function () {
-    //Load the images for level
-    //Load instructor gif
-    instructorGif = loader.loadImage("gif/instructor.gif", "instructorGif");
-    levelScreen.appendChild(instructorGif);
-
+    //Load the images and audio for playing level screen
+    //Assign them to the game object
+    //Load instructor GIF
+    game.instructorGif = loader.loadImage(
+      "gif/instructor.gif",
+      "instructorGif"
+    );
+    levelScreen.appendChild(game.instructorGif);
     //Load player images
-    playerImg = loader.loadImage("images/player1.png");
-    playerImgContainer.appendChild(playerImg);
-
+    game.playerImg = loader.loadImage("images/player1.png");
+    playerImgContainer.appendChild(game.playerImg);
     //Load the audio for level
     game.baseAudio = loader.loadSound("audio/base-audio.mp3", "baseAudio");
     levelScreen.appendChild(game.baseAudio);
@@ -188,6 +209,10 @@ const level = {
 //===============================
 //Scoreboard object
 //===============================
+
+//Dynamic objects: Dynamic objects are objects whose properties and methods can be added or removed at runtime.
+//You can create dynamic objects using object literals, and add or remove properties and methods using regular
+//object property assignment and delete, as shown in the following example:
 
 const scoreboard = {
   score: 0,
@@ -205,18 +230,15 @@ const scoreboard = {
         let endTime = Date.now();
         let elapsedTime = endTime - startTime;
         let roundedElapsedTime = Math.round(elapsedTime / 125) * 0.125;
-        console.log(roundedElapsedTime);
         if (preciseTimingArr.includes(roundedElapsedTime)) {
+          //Score dynamically updates during runtime
           let updatedScore = scoreboard.score++ + 1;
           document.getElementById("score").innerHTML =
             "Score:" + scoreboard.score + "/19";
+          //Player image dynamically updates during runtime by changing the image URL
           let updatedURL = URL++ + 1;
-          console.log(`URL: ${updatedURL}`);
           let player = document.getElementById("playerImg");
           player.innerHTML = '<img src="images/player' + updatedURL + '.png"/>';
-          console.log(`Score : ${updatedScore}`);
-        } else {
-          console.log("Missed!");
         }
       }
     });
@@ -224,25 +246,25 @@ const scoreboard = {
 };
 
 //===============================
-//Ending screen object
+//Ending object
 //===============================
 const ending = {
   init: function () {
-    // baseAudio = document.getElementById("baseAudio");
     winScreen = document.getElementById("winScreen");
     loseScreen = document.getElementById("loseScreen");
 
+    //Load assets and assign them to root game object
     //Load winning screen
-    winnerGif = loader.loadImage("gif/winningscreen.gif", "winnerGif");
-    winScreen.appendChild(winnerGif);
+    ending.winnerGif = loader.loadImage("gif/winningscreen.gif", "winnerGif");
+    winScreen.appendChild(ending.winnerGif);
 
     //Load winning cheer
     ending.cheer = loader.loadSound("audio/cheer.mp3", "cheer");
     winScreen.appendChild(ending.cheer);
 
     //Load losing screen
-    loserImg = loader.loadImage("images/losingscreen.png", "loserImg");
-    loseScreen.appendChild(loserImg);
+    ending.loserImg = loader.loadImage("images/losingscreen.png", "loserImg");
+    loseScreen.appendChild(ending.loserImg);
 
     //Load losing boo
     ending.boo = loader.loadSound("audio/boo.mp3", "boo");
@@ -257,6 +279,7 @@ const ending = {
     jsConfetti.addConfetti({ confettiNumber: 1000 });
   },
 
+  //Method to show either winning or losing screen base on the score
   winLose: function () {
     game.baseAudio.addEventListener("ended", function () {
       if (scoreboard.score > 10) {
@@ -272,5 +295,3 @@ const ending = {
     });
   },
 };
-
-console.log("no bug");
